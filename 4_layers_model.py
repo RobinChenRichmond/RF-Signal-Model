e12152ac0f801164962e064f2b72ad60a323bd75
RobinChenRichmond <guanyu.chen@richmond.edu>
2017-11-29 19:18:22 -0500

    update the new model

---
+++ 4_layers_model.py
@@ -0,0 +1,212 @@
+import os,random
+os.environ["KERAS_BACKEND"] = "tensorflow"
+import math
+import numpy as np
+from keras.utils import np_utils
+import keras.models as models
+from keras.layers.core import Reshape,Dense,Dropout,Activation,Flatten
+from keras.layers.noise import GaussianNoise
+from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D, Conv1D, MaxPooling1D, ZeroPadding1D
+from keras.regularizers import *
+from keras.optimizers import adam
+import matplotlib.pyplot as plt
+import cPickle, random, sys, keras
+
+
+# Load the dataset
+#  You will need to seperately download or generate this file
+Xd = cPickle.load(open("/Users/guanyuchen/Desktop/MPS_Project/RML2016.10a_dict.dat",'rb'))
+print("Dataset imported")
+snrs,mods = map(lambda j: sorted(list(set(map(lambda x: x[j], Xd.keys())))), [1,0])
+X = []  
+lbl = []
+for mod in mods:
+    for snr in snrs:
+        X.append(Xd[(mod,snr)])
+        for i in range(Xd[(mod,snr)].shape[0]):
+            lbl.append((mod,snr))
+
+X = np.vstack(X)
+#220000*2*128
+
+#dat = X[i,0,:]+1j*X[i,1,:]
+
+'''
+X2 = []
+for i in range(X.shape[0]):
+#  temp = []
+  X2.append(X[i,0,:]+1j*X[i,1,:])
+#  for k in range(X.shape[2]):
+#    temp.append(X[i,0,k]+1j*X[i,1,k])
+#    temp.append(math.cos(X[i,1,j])+math.sin(X[i,0,j]))
+#  X2.append(temp)
+
+X2 = np.vstack(X2)
+X = X2
+'''
+
+print(snrs)
+print(mods)
+print("Data prepared")
+
+
+
+
+# Partition the data
+#  into training and test sets of the form we can train/test on 
+#  while keeping SNR and Mod labels handy for each
+np.random.seed(2016)
+n_examples = X.shape[0]
+n_train = int(n_examples * 0.5)
+train_idx = np.random.choice(range(0,n_examples), size=n_train, replace=False)
+test_idx = list(set(range(0,n_examples))-set(train_idx))
+X_train = X[train_idx]
+X_test =  X[test_idx]
+def to_onehot(yy):
+    yy1 = np.zeros([len(yy), max(yy)+1])
+    yy1[np.arange(len(yy)),yy] = 1
+    return yy1
+
+Y_train = to_onehot(map(lambda x: mods.index(lbl[x][0]), train_idx))
+Y_test = to_onehot(map(lambda x: mods.index(lbl[x][0]), test_idx))
+
+in_shp = list(X_train.shape[1:])
+print X_train.shape, in_shp
+classes = mods
+
+# Build VT-CNN2 Neural Net model using Keras primitives -- 
+#  - Reshape [N,2,128] to [N,1,2,128] on input
+#  - Pass through 2 2DConv/ReLu layers
+#  - Pass through 2 Dense layers (ReLu and Softmax)
+#  - Perform categorical cross entropy optimization
+
+dr = 0.5 # dropout rate (%)
+model = models.Sequential()
+model.add(Reshape(in_shp+[1], input_shape=in_shp))
+model.add(ZeroPadding2D((0,2)))
+
+#original
+'''
+model.add(Conv2D(256, (1,3), activation="relu"))
+model.add(Dropout(dr))
+model.add(ZeroPadding2D((0,2)))
+model.add(Conv2D(80, (2,3), activation="relu"))
+model.add(Dropout(dr))
+'''
+
+#4layers
+model.add(Conv2D(64, (1,4), activation="relu"))
+model.add(Dropout(dr))
+model.add(ZeroPadding2D((0,2)))
+model.add(Conv2D(128, (2,4), activation="relu"))
+model.add(Dropout(dr))
+model.add(Conv2D(128, (1,8), activation="relu"))
+model.add(Dropout(dr))
+model.add(Conv2D(256, (1,8), activation="relu"))
+model.add(Dropout(dr))
+
+model.add(Flatten())
+model.add(Dense(256, activation='relu', name="dense1"))
+model.add(Dropout(dr))
+model.add(Dense( len(classes), activation='softmax', name="dense2" ))
+model.add(Reshape([len(classes)]))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.summary()
+
+# Set up some params 
+nb_epoch = 100     # number of epochs to train on
+batch_size = 1024  # training batch size
+
+
+filepath = 'weight_4layers.wts.h5'
+history = model.fit(X_train,
+    Y_train,
+    batch_size=batch_size,
+    nb_epoch=nb_epoch,
+    verbose=1,
+    validation_data=(X_test, Y_test),
+    callbacks = [
+        keras.callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=0, save_best_only=True, mode='auto'),
+        keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, verbose=0, mode='auto')
+    ])
+# we re-load the best weights once training is finished
+
+model.load_weights('weight_4layers.wts.h5')
+
+
+# Show simple version of performance
+score = model.evaluate(X_test, Y_test, verbose=0, batch_size=batch_size)
+print score
+
+# Show loss curves 
+plt.figure()
+plt.title('Training performance')
+plt.plot(history.epoch, history.history['loss'], label='train loss+error')
+plt.plot(history.epoch, history.history['val_loss'], label='val_error')
+plt.legend()
+
+
+def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues, labels=[]):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(labels))
+    plt.xticks(tick_marks, labels, rotation=45)
+    plt.yticks(tick_marks, labels)
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+# Plot confusion matrix
+test_Y_hat = model.predict(X_test, batch_size=batch_size)
+conf = np.zeros([len(classes),len(classes)])
+confnorm = np.zeros([len(classes),len(classes)])
+for i in range(0,X_test.shape[0]):
+    j = list(Y_test[i,:]).index(1)
+    k = int(np.argmax(test_Y_hat[i,:]))
+    conf[j,k] = conf[j,k] + 1
+
+for i in range(0,len(classes)):
+    confnorm[i,:] = conf[i,:] / np.sum(conf[i,:])
+
+plot_confusion_matrix(confnorm, labels=classes)
+
+
+# Plot confusion matrix
+acc = {}
+for snr in snrs:
+  # extract classes @ SNR
+  test_SNRs = map(lambda x: lbl[x][1], test_idx)
+  test_X_i = X_test[np.where(np.array(test_SNRs)==snr)]
+  test_Y_i = Y_test[np.where(np.array(test_SNRs)==snr)]    
+  # estimate classes
+  test_Y_i_hat = model.predict(test_X_i)
+  conf = np.zeros([len(classes),len(classes)])
+  confnorm = np.zeros([len(classes),len(classes)])
+
+  for i in range(0,test_X_i.shape[0]):
+    j = list(test_Y_i[i,:]).index(1)
+    k = int(np.argmax(test_Y_i_hat[i,:]))
+    conf[j,k] = conf[j,k] + 1
+
+  for i in range(0,len(classes)):
+    confnorm[i,:] = conf[i,:] / np.sum(conf[i,:])
+
+  plt.figure()
+  plot_confusion_matrix(confnorm, labels=classes, title="ConvNet Confusion Matrix (SNR=%d)"%(snr))
+  cor = np.sum(np.diag(conf))
+  ncor = np.sum(conf) - cor
+  print "Overall Accuracy: ", cor / (cor+ncor)
+  acc[snr] = 1.0*cor/(cor+ncor)
+
+# Save results to a pickle file for plotting later
+print acc
+fd = open('results_cnn2_d0.5.dat','wb')
+cPickle.dump( ("CNN2", 0.5, acc) , fd )
+
+# Plot accuracy curve
+plt.plot(snrs, map(lambda x: acc[x], snrs))
+plt.xlabel("Signal to Noise Ratio")
+plt.ylabel("Classification Accuracy")
+plt.title("CNN2 Classification Accuracy on RadioML 2016.10 Alpha")
